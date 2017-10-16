f6eb8f5a6619c15b84862eb30cac9c7310db99b1
AlexKaneRUS <Dongodfather@mail.ru>
2017-10-16 09:16:34 +0300

    lambda calculator

--- src/Construction/Internal/Types.hs
+++ src/Construction/Internal/Types.hs
@@ -1,12 +1,23 @@
 module Construction.Internal.Types
-  ( Name, Term(..)
+  ( Name, Term(..), ContextT, Type (..), ContextElement, TypeEquation, TypeEquationSystem
   ) where
 
-import Data.Text (Text) -- we want to import only Text from Data.Text.
+import           Data.Text (Text)
 
 
 
 type Name = Text -- just alias, no more
+
+type ContextT = [ContextElement]
+type ContextElement = (Name, Type)
+
+data Type
+  = VarType { varType  :: Name
+            }
+  | Arrow   { from :: Type
+            , to   :: Type
+            }
+            deriving (Show, Eq, Ord)
 
 data Term = Var { var :: Name }                     -- Variables: a, b, ...
           | App { algo :: Term, arg :: Term }       -- Application: M N
@@ -14,3 +25,6 @@ data Term = Var { var :: Name }                     -- Variables: a, b, ...
   deriving (Show, Eq, Ord) -- we deriving some common classes like Show.
                            -- With this deriving you can use function "show"
                            -- to print your term.
+
+type TypeEquation = (Type, Type)
+type TypeEquationSystem = [TypeEquation]
